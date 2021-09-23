c731c212ad5a723b129d662719fd34e914a10a4c
charlie1099 <cdcharlesworth@gmail.com>
2021-09-22 22:55:25 -0600

    player object and stats

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -1,9 +1,9 @@
-const { jest, expect } = require("@jest/globals");
+
 const Player = require("../lib/Player");
 
 const Potion = require("../lib/Potion")
 
-jest.mock("../lib/Potion");
+jest.mock("../lib/Potion.js");
 
 test("creats a player object", () => {
     const player = new Player("Dave");
@@ -14,6 +14,25 @@ test("creats a player object", () => {
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
-    );
+);
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player("Dave");
+
+    expect(player.getStats()).toHaveProperty("potions");
+    expect(player.getStats()).toHaveProperty("health");
+    expect(player.getStats()).toHaveProperty("strength");
+    expect(player.getStats()).toHaveProperty("agility");
+});
+
+test("gets inventory from player or returns False", () => {
+    const player = new Player("Dave");
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
 
